38aa8ba477fc96eb10d360a27ded91f210e89be6
Deepson Shrestha : Global <deepson.shrestha@mutualofomaha.com>
2022-02-14 19:01:22 -0600

    First addition

---
+++ 00-Intro/Hello_World.py
@@ -0,0 +1,9 @@
+
+print('Hello World!')
+
+print('I installed Python extension and completed a few cosmetic changes:')
+# Installed Code Runner extension. Now, clicking the play button runs the code with Code Runner which makes it a little nicer.
+# Settings -> Run code (code runner) -> checked clear console when you run code each time
+# Uncheck show exection message -> will not print out the directory information
+
+print('Code Runner cleans the output')
